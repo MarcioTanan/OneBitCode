5a1dfcb82cc0b8e62c05fce76df273ba57687798
Marcio Tanan <marcio-tanan@hotmail.com>
2024-03-13 09:29:09 -0300

    Exercicio recruta

---
+++ Javascript-Modulo-1/exercicio-1/recruta/index.js
@@ -0,0 +1,8 @@
+let primeiroNome  = prompt("Qual seu primeiro nome?")
+let sobreNome  = prompt("Qual seu sobrenome?")
+let campoEstudo  = prompt("Qual seu campo de estudo?")
+let nascimento  = prompt("Qual seu ano de nascimento")
+
+let data = 2024 - nascimento
+
+alert("Nome Completo: " + primeiroNome + " " + sobreNome + " " + "\nCampo de Estudo: " + campoEstudo + " " + "\nIdade: " + data + " anos") 
