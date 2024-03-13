0cf76f0647072e85eff4da7a40f9d3d3ea3b2108
Marcio Tanan <marcio-tanan@hotmail.com>
2024-03-13 09:36:29 -0300

    exercicio calculadora

---
+++ Javascript-Modulo-1/exercicio-1/calculadora/index.js
@@ -0,0 +1,12 @@
+let primeiroNumero = prompt("Digite o primeiro número: ")
+let segundoNumero = prompt("Digite o segundo número: ")
+
+let pnumero = parseFloat(primeiroNumero)
+let snumero = parseFloat(segundoNumero)
+
+let soma = pnumero + snumero
+let subtracao = pnumero - snumero
+let multiplicacao = pnumero * snumero
+let divisao = pnumero / snumero
+
+alert(`A soma dos números: ${soma}\nA subtracao dos números: ${subtracao}\nA multiplicacao dos números: ${multiplicacao}\nA divisao dos números: ${divisao}\n `)
