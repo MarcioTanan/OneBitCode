4b6b871683eff6b508cde210b2534418c69daca8
Marcio Tanan <marcio-tanan@hotmail.com>
2024-03-29 17:19:02 -0300

    update

--- Javascript-Modulo-2/exercicio-12/index.js
+++ Javascript-Modulo-2/exercicio-12/index.js
@@ -23,70 +23,90 @@ const form = document.getElementById("devForm");
 const developers = [];
 let inputRows = 0;
 
-
 // criar o evento do botão que adiciona os novos campos no formulário
-addTechBtn.addEventListener('click', function (ev) {
-    const stackInputs = document.getElementById('stackInputs')
-  
-    const newRow = document.createElement('li')
-    const rowIndex = inputRows
-    inputRows++
-    newRow.id = 'inputRow-' + rowIndex
-    newRow.className = 'inputRow'
-  
-    const techNameLabel = createLabel('Nome: ', 'techName-' + rowIndex)
-    const techNameInput = createInput('techName-' + rowIndex, null, 'techName')
-  
-    const expLabel = createLabel('Experiência: ')
-    const id1 = 'expRadio-' + rowIndex + '.1'
-    const expRadio1 = createInput(id1, '0-2 anos', 'techExp-' + rowIndex, 'radio')
-    const expLabel1 = createLabel('0-2 anos', id1)
-    const id2 = 'expRadio-' + rowIndex + '.2'
-    const expRadio2 = createInput(id2, '3-4 anos', 'techExp-' + rowIndex, 'radio')
-    const expLabel2 = createLabel('3-4 anos', id2)
-    const id3 = 'expRadio-' + rowIndex + '.3'
-    const expRadio3 = createInput(id3, '5+ anos', 'techExp-' + rowIndex, 'radio')
-    const expLabel3 = createLabel('5+ anos', id3)
-  
-    const removeRowBtn = document.createElement('button')
-    removeRowBtn.type = 'button'
-    removeRowBtn.innerText = 'Remover'
-    removeRowBtn.addEventListener('click', function () {
-      stackInputs.removeChild(newRow)
-    })
-  
-  
-    newRow.append(
-      techNameLabel, techNameInput, expLabel, expRadio1, expLabel1, expRadio2, expLabel2, expRadio3, expLabel3, removeRowBtn
-    )
-  
-    stackInputs.appendChild(newRow)
-  })
+addTechBtn.addEventListener("click", function (ev) {
+  const stackInputs = document.getElementById("stackInputs");
 
+  const newRow = document.createElement("li");
+  const rowIndex = inputRows;
+  inputRows++;
+  newRow.id = "inputRow-" + rowIndex;
+  newRow.className = "inputRow";
 
-  //adicionar o evento de submissão do formulário
-  form.addEventListener('submit', function (ev) {
-    ev.preventDefault()
-  
-    const fullnameInput = document.getElementById('fullname')
-    const inputRows = document.querySelectorAll('.inputRow')
-  
-    let technologies = []
-    inputRows.forEach(function (row) {
-      // #rowId input[name="techName"]
-      const techName = document.querySelector('#' + row.id + ' input[name="techName"]').value
-      const techExp = document.querySelector('#' + row.id + ' input[type="radio"]:checked').value
-      technologies.push({ name: techName, exp: techExp })
-    })
-  
-    const newDev = { fullname: fullnameInput.value, technologies: technologies }
-    developers.push(newDev)
-    alert('Dev cadastrado com sucesso!')
-  
-    fullnameInput.value = ''
-    inputRows.forEach(function (row) {
-      row.remove()
-    })
-  
-    console.log(developers)
-  })
+  const techNameLabel = createLabel("Nome: ", "techName-" + rowIndex);
+  const techNameInput = createInput("techName-" + rowIndex, null, "techName");
+
+  const expLabel = createLabel("Experiência: ");
+  const id1 = "expRadio-" + rowIndex + ".1";
+  const expRadio1 = createInput(
+    id1,
+    "0-2 anos",
+    "techExp-" + rowIndex,
+    "radio"
+  );
+  const expLabel1 = createLabel("0-2 anos", id1);
+  const id2 = "expRadio-" + rowIndex + ".2";
+  const expRadio2 = createInput(
+    id2,
+    "3-4 anos",
+    "techExp-" + rowIndex,
+    "radio"
+  );
+  const expLabel2 = createLabel("3-4 anos", id2);
+  const id3 = "expRadio-" + rowIndex + ".3";
+  const expRadio3 = createInput(id3, "5+ anos", "techExp-" + rowIndex, "radio");
+  const expLabel3 = createLabel("5+ anos", id3);
+
+  const removeRowBtn = document.createElement("button");
+  removeRowBtn.type = "button";
+  removeRowBtn.innerText = "Remover";
+  removeRowBtn.addEventListener("click", function () {
+    stackInputs.removeChild(newRow);
+  });
+
+  newRow.append(
+    techNameLabel,
+    techNameInput,
+    expLabel,
+    expRadio1,
+    expLabel1,
+    expRadio2,
+    expLabel2,
+    expRadio3,
+    expLabel3,
+    removeRowBtn
+  );
+
+  stackInputs.appendChild(newRow);
+});
+
+//adicionar o evento de submissão do formulário
+form.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+
+  const fullnameInput = document.getElementById("fullname");
+  const inputRows = document.querySelectorAll(".inputRow");
+
+  let technologies = [];
+  inputRows.forEach(function (row) {
+    // #rowId input[name="techName"]
+    const techName = document.querySelector(
+      "#" + row.id + ' input[name="techName"]'
+    ).value;
+    const techExp = document.querySelector(
+      "#" + row.id + ' input[type="radio"]:checked'
+    ).value;
+    technologies.push({ name: techName, exp: techExp });
+  });
+
+  const newDev = { fullname: fullnameInput.value, technologies: technologies };
+  developers.push(newDev);
+  alert("Dev cadastrado com sucesso!");
+
+  fullnameInput.value = "";
+  inputRows.forEach(function (row) {
+    row.remove();
+  });
+
+  console.log(developers);
+});
