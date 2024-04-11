2d166883c255981d627b2eb5507fe181bc5ea951
Marcio Tanan <marcio-tanan@hotmail.com>
2024-04-11 09:47:03 -0300

    npx e scripts

--- Javascript-Modulo-3/exercicio-16/birthday.js
+++ Javascript-Modulo-3/exercicio-16/birthday.js
@@ -8,7 +8,7 @@ function birthday(date) {
   const daysToNextBirthday = nextBirthday.diff(today, 'day') + 1
 
   console.log(`Idade: ${ageInYears}`)
-  console.log(`Próximo aniversário: ${nextBirthday.format('DD/MM/YYYY')}`)
+  console.log(`Próximo aniversário: ${nextBirthday.format('DD/MM/YY')}`)
   console.log(`Dias até completar ${ageInYears + 1} anos: ${daysToNextBirthday}`)
 }
 
